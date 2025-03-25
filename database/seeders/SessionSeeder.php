a8ca84d0b84960e5adbb51fd8989d1bdd8b0f85a
nika.movchanskaya <nika.movchanskaya@gmail.com>
2025-03-25 21:05:36 +0100

    feat: SessionSeeder

--- database/seeders/SessionSeeder.php
+++ database/seeders/SessionSeeder.php
@@ -14,17 +14,17 @@ class SessionSeeder extends Seeder
     public function run(): void
     {
         FilmSession::insert([
-            ['film_id' => 1, 'hall_id' => 1, 'date' => '2025-03-25', 'start_time' => '14:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 1, 'hall_id' => 1, 'date' => '2025-03-25', 'start_time' => '16:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 2, 'hall_id' => 1, 'date' => '2025-03-25', 'start_time' => '20:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 3, 'hall_id' => 2, 'date' => '2025-03-25', 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 3, 'hall_id' => 2, 'date' => '2025-03-25', 'start_time' => '15:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 3, 'hall_id' => 1, 'date' => '2025-03-25', 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 1, 'hall_id' => 2, 'date' => '2025-03-30', 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 2, 'hall_id' => 2, 'date' => '2025-03-26', 'start_time' => '16:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 3, 'hall_id' => 1, 'date' => '2025-03-27', 'start_time' => '18:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 4, 'hall_id' => 2, 'date' => '2025-03-28', 'start_time' => '20:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
-            ['film_id' => 5, 'hall_id' => 1, 'date' => '2025-03-29', 'start_time' => '22:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 1, 'hall_id' => 1, 'date' => date('Y-m-d'), 'start_time' => '14:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 1, 'hall_id' => 1, 'date' => date('Y-m-d'), 'start_time' => '16:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 2, 'hall_id' => 1, 'date' => date('Y-m-d'), 'start_time' => '20:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 3, 'hall_id' => 2, 'date' => date('Y-m-d'), 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 3, 'hall_id' => 2, 'date' => date('Y-m-d'), 'start_time' => '15:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 3, 'hall_id' => 1, 'date' => date('Y-m-d'), 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 1, 'hall_id' => 2, 'date' => now()->addDay()->toDateString(), 'start_time' => '12:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 2, 'hall_id' => 2, 'date' => now()->addDay()->toDateString(), 'start_time' => '16:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 3, 'hall_id' => 1, 'date' => now()->addDays(2)->toDateString(), 'start_time' => '18:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 4, 'hall_id' => 2, 'date' => now()->addDays(2)->toDateString(), 'start_time' => '20:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
+            ['film_id' => 5, 'hall_id' => 1, 'date' => now()->addDays(2)->toDateString(), 'start_time' => '22:00', 'status' => 'active', 'created_at' => now(), 'updated_at' => now()],
         ]);
     }
 }
